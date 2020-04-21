9352adc4c638b0a9add3c621081e1dc651bda865
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-04-20 22:41:29 -0400

    yeaa

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+
+function initMap() {
+    var sydney =
+    {
+        lat: -33.863276,
+        lng: 151.107977
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: sydney,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+
+    });
+}
+
+function showStoreMarker(){
+    
+}
